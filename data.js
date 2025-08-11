c1f4c4b6eba9879405707fd9d6f4f2cc44336dec
pip8989 <morningdew10000@gmail.com>
2025-08-11 16:41:19 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,20 @@
+export const artData = [{
+    title: 'Fall Air',
+    src: 'images/fallairthumbnail.jpg',
+    medium: 'Oil on board 8x10 inch',
+    alt: 'fallair'
+},
+
+{
+    title: 'Green Lands',
+    src: 'images/greenlandsthumbnail.jpg',
+    medium: 'Oil on canvas 12x12 inch',
+    alt: 'greenlands'
+},
+{
+    title: 'Salt Lake',
+    src: 'images/saltlakethumbnail.jpg',
+    medium: 'Oil on canvas 12x12 inch',
+    alt: 'saltlake'
+}
+]
---
+++ index.js
@@ -0,0 +1,53 @@
+import { artData } from './data.js';
+console.log(artData.name)
+
+const lightbox = document.getElementById('lightbox');
+const lightboxImage = document.getElementById('lightbox-image');
+const main = document.querySelector('main');
+const closeBtn = document.getElementById('close-btn');
+
+
+// Image links for full-size images
+const fullSizeImages = {
+    'fallair': 'images/fallair.jpg',
+    'greenlands': 'images/greenlands.jpg',
+    'saltlake': 'images/saltlake.jpg'
+};
+
+let html = ``
+artData.forEach((item) => {
+    html += `
+     <a href="#" class="image-card gallery-item">
+        <article>
+            <img src=${item.src} alt=${item.alt}>
+                <div class="article-text">
+                    <h2>${item.title}</h2>
+                    <p>${item.medium}</p>
+                </div>
+        </article>
+    </a>
+    `
+
+})
+main.innerHTML = html
+
+const galleryItemArr = document.querySelectorAll('.gallery-item');
+
+galleryItemArr.forEach(item => {
+    item.addEventListener('click', (e) => {
+        const altText = e.target.alt.toLowerCase();
+        lightbox.style.display = 'flex'; // Show lightbox
+        lightboxImage.src = fullSizeImages[altText]; // Set full-size image source
+    });
+});
+
+// Close the lightbox when clicking the close button or outside the image
+closeBtn.addEventListener('click', () => {
+    lightbox.style.display = 'none'; // Hide lightbox
+});
+
+lightbox.addEventListener('click', (e) => {
+    if (e.target === lightbox) {
+        lightbox.style.display = 'none'; // Hide lightbox if clicking outside the image
+    }
+});
