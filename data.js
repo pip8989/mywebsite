43ea5ff664119621aa7ef65ff4351cec3dc7104c
pip8989 <morningdew10000@gmail.com>
2025-08-16 16:49:01 -0600

    Add files via upload

--- data.js
+++ data.js
@@ -1,44 +1,44 @@
 export const artData = [{
     title: 'Fall Air',
     src: 'images/fallairthumbnail.jpg',
-    medium: 'Oil on board 8x10 inch',
+    medium: 'Oil on board 8x10 inch $400',
     alt: 'fallair'
 },
 
 {
     title: 'Green Lands',
     src: 'images/greenlandsthumbnail.jpg',
-    medium: 'Oil on canvas 12x12 inch',
+    medium: 'Oil on canvas 12x12 inch $550',
     alt: 'greenlands'
 },
 {
     title: 'Salt Lake',
     src: 'images/saltlakethumbnail.jpg',
-    medium: 'Oil on canvas 12x12 inch',
+    medium: 'Oil on canvas 12x12 inch $550',
     alt: 'saltlake'
 },
 {
     title: 'Blue Days',
     src: 'images/bluedaysthumbnail.jpg',
-    medium: 'Oil on canvas 8x10 inch',
+    medium: 'Oil on canvas 8x10 inch $400',
     alt: 'bluedays'
 },
 {
     title: 'Blue Days composition 2',
     src: 'images/bluedaycomp2thumbnail.jpg',
-    medium: 'Oil on canvas 8x10 inch',
+    medium: 'Oil on canvas 8x10 inch $400',
     alt: 'bluedaycomp2'
 },
 {
     title: 'Peace Lake',
     src: 'images/peacelakethumbnail.jpg',
-    medium: 'Oil on canvas 8x10 inch',
+    medium: 'Oil on canvas 8x10 inch $400',
     alt: 'peacelake'
 },
 {
     title: 'Evening',
     src: 'images/eveningthumbnail.jpg',
-    medium: 'Oil on canvas 8x10 inch',
+    medium: 'Oil on canvas 8x10 inch $400',
     alt: 'evening'
 }
 
